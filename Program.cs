aee153ba936d49da4f696522c4d807417d3cbb1a
MyllyAnni <annimyllyniemi7@msn.com>
2025-09-08 09:07:14 +0300

    Alkulätinät

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.Sqlite;
+
+namespace KauppakantaTunnilla;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
