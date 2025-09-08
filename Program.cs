73dca80a44a784017c8306e7acb35a319e9ea0c5
MyllyAnni <annimyllyniemi7@msn.com>
2025-09-08 10:35:30 +0300

    Lisätty gitignore ja programiin koodia

--- Program.cs
+++ Program.cs
@@ -1,9 +1,43 @@
-﻿using Microsoft.Data.Sqlite;
+﻿namespace KauppakantaTunnilla;
 
-namespace KauppakantaTunnilla;
-
-public class KauppaDB
+using Microsoft.Data.Sqlite;
+class Program
 {
-    private string _connetctionstring = "Data Source = kauppa.db";
-    
+    static void Main(string[] args)
+    {
+        KauppaDB kauppaDB = new KauppaDB();
+
+        while (true)
+        {
+            Console.WriteLine("Haluatko lisätä tuotteen (L), hakea (H) vai lopettaa (X)?");
+            string? vastaus = Console.RaéadLine();
+            switch(vastaus)
+
+            case "L":
+                Console.WriteLine("Anna tuotteen nimi:");
+                string? nimi = Console.ReadLinew(); 
+
+                Console.WriteLine("Mikä sen tuotteen hinta on?");
+                double hinta = Convert.ToDouble(Console.ReadLine());
+
+                // Lisätään tuote.
+                kauppaDB.LisaaTuoe(nimi, hinta);
+                break;
+
+                case "H":
+                    // Hae tuote tietokannasta.
+                    Console.WriteLine("Anna haettavan tuotteen nimi:");
+                    string? haettavaNimi = Console.ReadLine();
+                    string tuotteet = kauppaDB.HaeTuotteet(haettavaNimi);
+                    Console.WriteLine(tuotteet);
+                    break;
+
+
+                case "X":
+                    return;
+            default:
+                Console.WriteLine("Väärä valinta. aNNA L, H tai X.");
+                break;
+        }
+    }
 }
